f80ef351a7a69d323aa85429d5d7a4b9633202da
Nuhansa Rathnayake <nuhansarathnayake08@gmail.com>
2025-08-31 20:14:17 +0530

    documentation

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,6 +1,8 @@
 import { Link, Stack } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
 
+// NotFoundScreen: Displays a message when a user navigates to a non-existent route
+
 export default function NotFoundScreen() {
   return (
     <>
